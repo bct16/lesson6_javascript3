fce1e0821c02519e9ee03b3530967579ec787b10
Chris Thornton <bct16@my.fsu.edu>
2018-11-20 21:56:09 +0000

    lesson 6 final commit

--- photos.js
+++ photos.js
@@ -1,8 +1,8 @@
 
 /*    
     Program Name:  Photo Gallery Application
-    Author: 
-    Date:   
+    Author: Byron Thornton 
+    Date:   11/20/2018
     Filename: photos.js
     
  */
@@ -18,7 +18,22 @@ var figureCount = 3;
 /* This method adds src values to img elements based on order specified in photoOrder array             */
 /********************************************************************************************************/
 function populateFigures() {
-
+    var filename;
+    var currentFig;
+    
+    if (figureCount === 3) {
+     for (var i = 1; i < 4; i++) {
+ filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+ currentFig = document.getElementsByTagName("img")[i - 1];
+ currentFig.src = filename;
+ }//end of for loop
+} else {
+ for (var i = 0; i < 5; i++) {
+ filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+ currentFig = document.getElementsByTagName("img")[i];
+ currentFig.src = filename;
+ }//end of for loop
+ }//end of else
 
 
 
@@ -69,7 +84,39 @@ function leftArrow() {
 /*           This method is executed to allow the photo gallery to shift to five image layout.         */
 /******************************************************************************************************/
 function previewFive() {
+    var articleEl = document.getElementsByTagName("article")[0];
+    
+    var lastFigure = document.createElement("figure");
+    
+   lastFigure.id = "fig5";
+   lastFigure.style.zIndex = "5";
+   lastFigure.style.position = "absolute";
+   lastFigure.style.right = "45px";
+   lastFigure.style.top = "67px";
    
+   var lastImage = document.createElement("img");
+   lastImage.width = "240";
+   lastImage.height = "135";
+   
+   lastFigure.appendChild(lastImage);
+   articleEl.appendChild(lastFigure);
+   
+   var firstFigure = lastFigure.cloneNode(true);
+   
+   firstFigure.id = "fig1";
+   firstFigure.style.right = " ";
+   firstFigure.style.left = "45px";
+   
+   articleEl.insertBefore(firstFigure, document.getElementById("fig2"));
+    
+    
+    document.getElementsByTagName("img")[0].src = "images/IMG_0" + photoOrder[0] + "sm.jpg";
+    document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
+    
+    figureCount = 5;
+    
+    var numberButton = document.querySelector("#fiveButton p");
+    numberButton.removeEventListener("click", previewFive, false);
 
    
    
@@ -83,9 +130,14 @@ function previewFive() {
 /*         This method attaches event listeners to the buttons (left, right, and show all).            */
 /*******************************************************************************************************/
 function createEventListeners() {
-   
+    var leftarrow = document.getElementById("leftarrow");
+    leftarrow.addEventListener("click", leftArrow, false);
+    
+    var rightarrow = document.getElementById("rightarrow");
+    rightarrow.addEventListener("click", rightArrow, false);
 
-
+   var showAllButton = document.querySelector("#fiveButton p");
+   showAllButton.addEventListener("click", previewFive, false);
    
 }//end of createEventListeners function
 
